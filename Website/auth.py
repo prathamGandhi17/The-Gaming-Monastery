2d68d0691bf6439e009a321ebf1f27e2ed60acc5
prathamGandhi17 <pragandhi@algomau.ca>
2025-04-16 21:54:01 -0400

    Modified functionalities: improved ui, made to play tab work

--- Website/auth.py
+++ Website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 #urls
 auth = Blueprint('auth', __name__)
@@ -15,15 +16,19 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.main'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -36,9 +41,11 @@ def sign_up():
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists!', category='error')
+            return render_template("sign_up.html", user=current_user)
         user = User.query.filter_by(username=username).first()
         if user:
             flash('Username already exists!', category='error')
+            return render_template("sign_up.html", user=current_user)
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(email) > 150:
@@ -57,6 +64,7 @@ def sign_up():
                 new_user = User(email=email, username=username, password=generate_password_hash(password1))
                 db.session.add(new_user)
                 db.session.commit()
+                login_user(new_user, remember=True)
                 flash('Account created!', category='success')
-                return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+                return redirect(url_for('views.main'))
+    return render_template("sign_up.html", user=current_user)
--- Website/views 2.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-#urls
-views = Blueprint('views', __name__)
-
-#root
-@views.route('/')
-def home():
-    return render_template("home.html")
